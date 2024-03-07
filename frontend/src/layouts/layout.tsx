0576e9393bab681af92a332d9a7b21c2aafa9e96
greghim53lf <greghim53lf+github@gmail.com>
2024-03-07 10:29:08 +0100

    layout and header update
    Added two components from 'shadcn' for mobile responsive nav and also installed 'lucide-react' package for the hamburger icon

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/Header";
+
 type Props = {
   children: React.ReactNode;
 };
 
-export default function layout({ children }: Props) {
+export default function Layout({ children }: Props) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
