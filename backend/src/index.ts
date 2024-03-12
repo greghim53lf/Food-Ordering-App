e1ec9124670458c47f3c8eda7d05ba4d37ef8472
greghim53lf <greghim53lf+github@gmail.com>
2024-03-12 18:56:30 +0100

    create user api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,24 @@
 import express, { Request, Response } from "express";
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 import cors from "cors";
-app.use(cors())
+app.use(cors());
 
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRouter from "./routes/user";
 
-mongoose
-.connect(process.env.MONGODB_CONNECTION_STRING as string)
-.then(()=> {
-    console.log("Connected to datbase");
-    
-})
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
+  console.log("Connected to datbase");
+});
 
-app.get('/', async(req: Request ,res: Response) => {
-    res.json({message: 'Food Ordering App'})
-})
+app.use("/api/user", userRouter);
+
+app.get("/", async (req: Request, res: Response) => {
+  res.json({ message: "Food Ordering App" });
+});
 
 app.listen(3000, () => {
-    console.log('Server listening on port 3000');
-})
+  console.log("Server listening on port 3000");
+});
