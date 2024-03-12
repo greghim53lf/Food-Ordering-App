413948ffeba1fd83919769dc5063c09606b304ad
greghim53lf <greghim53lf+github@gmail.com>
2024-03-12 09:44:00 +0100

    authenticated links

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,4 +1,4 @@
-import { Menu } from "lucide-react";
+import { CircleUserRound, Menu } from "lucide-react";
 import {
   Sheet,
   SheetContent,
@@ -8,8 +8,12 @@ import {
 } from "./ui/sheet";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
 
 export default function MobileNav() {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+
   return (
     <Sheet>
       <SheetTrigger>
@@ -18,10 +22,25 @@ export default function MobileNav() {
       <SheetContent className="space-y-3">
         <SheetTitle>
           <span className="text-lime-600">Stay Green, Stay Fresh...</span>
+          {isAuthenticated && (
+            <span className="flex items-center font-bold gap-2">
+              <CircleUserRound className="text-lime-600" />
+              {user?.email}
+            </span>
+          )}
         </SheetTitle>
         <Separator />
-        <SheetDescription className="flex">
-          <Button className="flex-1 font-bold bg-lime-600">Login</Button>
+        <SheetDescription className="flex flex-col gap-4">
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button
+              onClick={async () => loginWithRedirect()}
+              className="flex-1 font-bold bg-lime-600"
+            >
+              Login
+            </Button>
+          )}
         </SheetDescription>
       </SheetContent>
     </Sheet>
