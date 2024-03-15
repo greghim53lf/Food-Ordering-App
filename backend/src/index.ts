de2e19134adee1f65ab30bc11be8c8f4ff099c10
greghim53lf <greghim53lf+github@gmail.com>
2024-03-15 09:30:31 +0100

    created frontend and backend git repos to host on render.com

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,11 +13,15 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("Connected to datbase");
 });
 
-app.use("/api/my/user", userRouter);
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
 
 app.get("/", async (req: Request, res: Response) => {
-  res.json({ message: "Food Ordering App" });
+  res.json({ message: "Greenie App" });
 });
+
+app.use("/api/my/user", userRouter);
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
